574465e6210743e1386d997c62d3832846aeee82
Prasanna Donga <Prasanna.Donga@godigit.com>
2024-08-02 10:10:30 +0530

    Non repeating

--- src/NonRepeatingCharacter.java
+++ src/NonRepeatingCharacter.java
@@ -1,8 +1,38 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class NonRepeatingCharacter {
+    public  static void nonRepeatingCharacter(LinkedHashMap<Character, Integer> map,String str){
+        char[] charArr=str.toCharArray();
+        for(char c:charArr){
+            if(map.containsKey(c)){
+                map.put(c, map.get(c)+1);
+
+            }
+            else {
+                map.put(c,1);
+            }
+        }
+        for(Map.Entry<Character, Integer> e:map.entrySet()){
+            if(e.getValue()==1){
+                System.out.println(e.getKey()+ " : "+e.getValue());
+                break;
+            }
+
+        }
+
+    }
+
     public static void main(String[] args) {
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
         Scanner sc= new Scanner(System.in);
+        String str= sc.nextLine();
+        nonRepeatingCharacter(map, str);
+
+
+
 
     }
 }
